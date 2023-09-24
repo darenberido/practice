b06510888f71279993c016c3de4cbebb415fffd3
Daren <darenberido@gmail.com>
2023-09-25 00:04:08 +0800

    comment

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Motortrade Life and Livelihood Assistance Foundation, Inc.</title>
+    <link rel="icon" type="images/x-icon" href="img/logo.png">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link href="https://fonts.cdnfonts.com/css/garet" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/gilroy-bold" rel="stylesheet">
+</head>
+<body>
+    <img src="img/mainBg.png" class="imgBg" alt="MLALAF BACKGROUND">
+    <div class="navigation">
+        <img src="img/logo.png" alt="MLALAF LOGO">
+        <ul>
+            <li><a class="active">Home</a></li>
+            <li><a href="scholarships.php" target="_self">Scholarships</a></li>
+            <li><a href="aboutUs.php">About Us</a></li>
+            <li><a href="contactUs.php">Contact Us</a></li>
+        </ul>
+    </div>
+    <div class="home">
+        <pre>
+            Uncover tailored
+            opportunities and pave
+            the way for a limitless
+            future. <span>Explore, apply, and
+            thrive</span> - your journey 
+            begins here.
+        </pre>
+        <div class="button">
+            <a href="account/login.php" class="in">Sign In</a>
+            <a href="account/signUp.php" class="up">Sign Up</a>
+        </div>
+    </div>
+</body>
+</html>
